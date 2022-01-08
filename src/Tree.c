879eb65eda63e3bd32b9735f8ba3991877172045
aderylo <a.m.derylo@gmail.com>
2022-01-09 00:47:23 +0100

    Basic tree

--- src/Tree.c
+++ src/Tree.c
@@ -1,3 +1,26 @@
 #include <errno.h>
+#include <string.h>
 
 #include "Tree.h"
+
+struct Tree
+{
+    char *name;
+};
+
+Tree *tree_new()
+{
+    Tree *tree = malloc(sizeof(Tree));
+    if (!tree)
+        return NULL;
+
+    tree->name = malloc(2 * sizeof(char));
+    strcpy(tree->name, "/");
+    return tree;
+}
+
+void tree_free(Tree *tree)
+{
+    free(tree->name);
+    free(tree);
+}
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "HashMap.h"
+#include "Tree.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,30 +7,34 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void print_map(HashMap* map) {
-    const char* key = NULL;
-    void* value = NULL;
+void print_map(HashMap *map)
+{
+    const char *key = NULL;
+    void *value = NULL;
     printf("Size=%zd\n", hmap_size(map));
     HashMapIterator it = hmap_iterator(map);
-    while (hmap_next(map, &it, &key, &value)) {
+    while (hmap_next(map, &it, &key, &value))
+    {
         printf("Key=%s Value=%p\n", key, value);
     }
     printf("\n");
 }
 
-
 int main(void)
 {
-    HashMap* map = hmap_new();
+    HashMap *map = hmap_new();
     hmap_insert(map, "a", hmap_new());
     print_map(map);
 
-    HashMap* child = (HashMap*)hmap_get(map, "a");
+    HashMap *child = (HashMap *)hmap_get(map, "a");
     hmap_free(child);
     hmap_remove(map, "a");
     print_map(map);
 
     hmap_free(map);
+
+    Tree *tree = tree_new();
+    tree_free(tree);
 
     return 0;
 }
