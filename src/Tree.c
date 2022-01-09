285c4bf8239ae124e27ff46abb5d6233b6e922af
aderylo <a.m.derylo@gmail.com>
2022-01-09 01:07:20 +0100

    Add hmap to Tree

--- src/Tree.c
+++ src/Tree.c
@@ -1,11 +1,14 @@
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "Tree.h"
+#include "HashMap.h"
 
 struct Tree
 {
     char *name;
+    HashMap *sub_trees;
 };
 
 Tree *tree_new()
@@ -14,6 +17,7 @@ Tree *tree_new()
     if (!tree)
         return NULL;
 
+    tree->sub_trees = hmap_new();
     tree->name = malloc(2 * sizeof(char));
     strcpy(tree->name, "/");
     return tree;
@@ -21,6 +25,16 @@ Tree *tree_new()
 
 void tree_free(Tree *tree)
 {
+
+    HashMapIterator it = hmap_iterator(tree->sub_trees);
+    const char *key;
+    Tree *node = NULL;
+    while (hmap_next(tree->sub_trees, &it, &key, &node))
+    {
+        tree_free(node);
+    }
+
+    hmap_free(tree->sub_trees);
     free(tree->name);
     free(tree);
 }
